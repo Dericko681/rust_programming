d3ed179f4a1980d637c4d8a21daeec7c4c4de8ec
dericko681 <abonghoderick@gmial.com>
2024-10-01 05:20:43 +0100

    understanding data types

---
+++ rust_basics/data_types.rs
@@ -0,0 +1,12 @@
+fn
+main(){
+    let name_string = "Dericko";
+    let age_float = 20;
+    let smart_bool =  true;
+    let height_float = 1.80;
+
+    println!("His name is{}", name_string );
+    println!("I am {} years old", age_float);
+    println!("i am a smart guy {}", smart_bool);
+    println!("i am {} metres tall", height_float);
+}
